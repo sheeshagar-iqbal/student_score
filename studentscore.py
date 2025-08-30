2eb3b867536f0da352b846dc388d63abd947d473
sheeshagar-iqbal <iqbalkhan234786@gmail.com>
2025-08-30 10:58:05 +0530

    recreate score2 file

--- studentscore.py
+++ studentscore.py
@@ -1,28 +1,33 @@
 import streamlit as st
 import numpy as np
 import joblib
+import pandas as pd
+import pickle
 
+# --- Load the model ---
+with open("score2.pkl", "rb") as f:
+    loaded_model = pickle.load(f)
 # Load model and scaler
-model = joblib.load("exam_score.pkl")
+# model = joblib.load("exam_score.pkl")
 scaler = joblib.load("scaler.pkl")
 
 st.title("📊 Student Performance Prediction")
 
 # Inputs
-age = st.number_input("Age", min_value=10, max_value=30, value=18)
+age = st.number_input("Age", min_value=10, max_value=25, value=18)
 gender = st.selectbox("Gender", ["male", "female", "other"])
-study_hours = st.slider("Study Hours per Day", 0, 12, 3)
-social_media = st.slider("Social Media Hours", 0, 12, 2)
-netflix = st.slider("Netflix Hours and TV Hours", 0, 12, 1)
+study_hours_per_day = st.slider("Study Hours per Day", 0, 8, 3)
+social_media_hours = st.slider("Social Media Hours", 0, 8, 2)
+netflix_hours = st.slider("Netflix & TV Hours", 0, 5, 1)
 part_time_job = st.selectbox("Part-time Job", ["yes", "no"])
-attendance = st.slider("Attendance (%)", 0, 100, 75)
-sleep = st.slider("Sleep Hours", 0, 12, 7)
-diet = st.selectbox("Diet Quality", ["poor", "good", "fair"])
-exercise = st.slider("Exercise Frequency (days/week)", 0, 6, 3)
-parent_edu = st.selectbox("Parental Education Level", ["highschool", "bachelor", "master"])
-internet = st.selectbox("Internet Quality", ["poor", "average", "good"])
-mental_health = st.slider("Mental Health Rating (1-10)", 1, 10, 5)
-extracurricular = st.selectbox("Extracurricular Participation", ["yes", "no"])
+attendance_percentage = st.slider("Attendance (%)", 50, 100, 75)
+sleep_hours = st.slider("Sleep Hours", 0, 8, 6)
+diet_quality = st.selectbox("Diet Quality", ["poor", "good", "fair"])
+exercise_frequency = st.slider("Exercise Frequency (day/week)", 0, 6, 3)
+parental_education_level = st.selectbox("Parental Education Level", ["highschool", "bachelor", "master"])
+internet_quality = st.selectbox("Internet Quality", ["poor", "average", "good"])
+mental_health_rating = st.slider("Mental Health Rating (1-10)", 1, 10, 5)
+extracurricular_participation = st.selectbox("Extracurricular Participation", ["yes", "no"])
 
 # 🔹 Encoding using mapping dictionaries
 gender_map = {"male": 1, "female": 0, "other": 2}
@@ -35,10 +40,10 @@ extra_map = {"no": 0, "yes": 1}
 # Apply mappings
 gender = gender_map[gender]
 part_time_job = job_map[part_time_job]
-diet = diet_map[diet]
-parent_edu = edu_map[parent_edu]
-internet = internet_map[internet]
-extracurricular = extra_map[extracurricular]
+diet_quality = diet_map[diet_quality]
+parental_education_level = edu_map[parental_education_level]
+internet_quality = internet_map[internet_quality]
+extracurricular_participation = extra_map[extracurricular_participation]
 
 
 
@@ -48,14 +53,37 @@ extracurricular = extra_map[extracurricular]
 #        'parental_education_level', 'internet_quality', 'mental_health_rating',
 #        'extracurricular_participation']
 # Collect into array
-features = np.array([[age, gender, study_hours, social_media, netflix,
-                      part_time_job, attendance, sleep, diet, exercise,
-                      parent_edu, internet, mental_health, extracurricular]])
+
+data ={
+    'age':age,
+    'gender':gender,
+    'study_hours_per_day':study_hours_per_day,
+    'social_media_hours': social_media_hours,
+
+    'netflix_hours':netflix_hours,
+    'part_time_job':part_time_job,
+    'attendance_percentage':attendance_percentage,
+
+    'sleep_hours':sleep_hours,
+    'diet_quality':diet_quality,
+    'exercise_frequency':exercise_frequency,
+
+    'parental_education_level':parental_education_level,
+    'internet_quality':internet_quality,
+    'mental_health_rating':mental_health_rating,
+    'extracurricular_participation':extracurricular_participation
+}
+features = pd.DataFrame([data])
 
 # Scale numeric
-features = scaler.fit_transform(features)
+numeric_cols = ["age", "study_hours_per_day", "social_media_hours",
+                "netflix_hours", "attendance_percentage",
+                "sleep_hours", "exercise_frequency", "mental_health_rating"]
+
+features[numeric_cols] = scaler.transform(features[numeric_cols])
 
 # Predict
 if st.button("Predict"):
-    prediction = model.predict(features)
+    prediction = loaded_model.predict(features)
+    prediction = np.clip(prediction, 0, 100)
     st.success(f"🎯 Predicted Output: {prediction[0]}")
